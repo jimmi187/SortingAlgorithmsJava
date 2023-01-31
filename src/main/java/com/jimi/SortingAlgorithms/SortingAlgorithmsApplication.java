1efaa0b01079bcf6454110f3a38d4fc0845349ee
jimmi187 <project.jimi@proton.me>
2023-01-31 22:54:11 +0100

    first commit

---
+++ src/main/java/com/jimi/SortingAlgorithms/SortingAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.jimi.SortingAlgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimi/SortingAlgorithms/SortingAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimi.SortingAlgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
