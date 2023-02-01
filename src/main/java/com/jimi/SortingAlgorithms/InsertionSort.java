3e72e2196f625edab71c683480ee5266b1d93ecf
jimmi187 <project.jimi@proton.me>
2023-02-01 13:55:15 +0100

    Insertion Sort added stream method and recursion

--- src/main/java/com/jimi/SortingAlgorithms/InsertionSort.java
+++ src/main/java/com/jimi/SortingAlgorithms/InsertionSort.java
@@ -1,5 +1,7 @@
 package com.jimi.SortingAlgorithms;
 
+import java.util.stream.IntStream;
+
 public class InsertionSort<T extends Comparable<T>>{
     private final T[] arr;
 
@@ -16,6 +18,31 @@ public class InsertionSort<T extends Comparable<T>>{
                 arr[i - 1] = temp;
                 i--;
             }
+        }
+    }
+
+    public void sortWithStreams() {
+        IntStream.range(1, arr.length)
+                .forEach(i ->
+                        IntStream.iterate(i, j -> j > 0 && arr[j].compareTo(arr[j - 1]) < 0, j -> j - 1)
+                                .forEach(j -> {
+                                    T temp = arr[j];
+                                    arr[j] = arr[j - 1];
+                                    arr[j - 1] = temp;
+                                })
+                );
+    }
+
+    public void sortRecursively() {
+        IntStream.range(1, arr.length).forEach(this::sort);
+    }
+
+    private void sort(int index) {
+        if (index > 0 && arr[index].compareTo(arr[index - 1]) < 0) {
+            T temp = arr[index];
+            arr[index] = arr[index - 1];
+            arr[index - 1] = temp;
+            sort(index - 1);
         }
     }
 }
