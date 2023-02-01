ff5cc9e1e15edf9d1dec49d2ddd12066436c9ba9
jimmi187 <project.jimi@proton.me>
2023-02-01 13:46:13 +0100

    Insertion Sort

---
+++ src/main/java/com/jimi/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,23 @@
+package com.jimi.SortingAlgorithms;
+
+public class InsertionSort<T extends Comparable<T>>{
+    private final T[] arr;
+
+    public InsertionSort(T[] arr) {
+        this.arr = arr;
+    }
+
+    public void sort(){
+        for(int j = 1 ; j<arr.length-1;j++) { // poredi se sa predhodnim i za svaki radi bubble unazad
+            for (int i = j; i >0; i--) {
+                if (arr[i].compareTo(arr[i - 1]) < 0) {
+                    T temp = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
