0f1b5409e052469dcbb464aba9ad03ae0f71fc9d
jimmi187 <project.jimi@proton.me>
2023-02-01 13:51:15 +0100

    Insertion Sort improved with while , fixed error on 11th line

--- src/main/java/com/jimi/SortingAlgorithms/InsertionSort.java
+++ src/main/java/com/jimi/SortingAlgorithms/InsertionSort.java
@@ -8,13 +8,13 @@ public class InsertionSort<T extends Comparable<T>>{
     }
 
     public void sort(){
-        for(int j = 1 ; j<arr.length-1;j++) { // poredi se sa predhodnim i za svaki radi bubble unazad
-            for (int i = j; i >0 && arr[i].compareTo(arr[i - 1]) < 0; i--) {
-
+        for(int j = 1 ; j<arr.length;j++) { // poredi se sa predhodnim i za svaki radi bubble unazad
+            int i = j;
+            while(i >0 && arr[i].compareTo(arr[i - 1]) < 0) {
                 T temp = arr[i];
                 arr[i] = arr[i - 1];
                 arr[i - 1] = temp;
-
+                i--;
             }
         }
     }
